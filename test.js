720fa20e305ea7ec9b63e623112f274b5de1ee4e
Shweta Singh <singh.shweta2973@gmail.com>
2024-09-13 21:12:30 +0530

    1st

--- test.js
+++ test.js
@@ -1,1 +1,7 @@
 console.log("shweta");
+// git init
+// git add .
+// git remote add origin https://github.com/ShwetaCodeCraft/javascript.git
+// git add -A
+// git commit -am "1st"
+// git push origin master
